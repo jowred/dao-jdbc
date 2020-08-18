a3eed368115ac69f25d1d2b1a61b0d90e0a2313c
jowred <32473283+jowred@users.noreply.github.com>
2020-08-17 22:49:28 -0300

    Criação das interfaces DAO

---
+++ src/main/java/model/dao/SellerDAO.java
@@ -0,0 +1,19 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Seller;
+
+public interface SellerDAO {
+
+	void insert(Seller s);
+	
+	void update(Seller s);
+	
+	void deleteById(Integer id);
+	
+	Seller findById(Integer id);
+	
+	List<Seller> findAll();
+	
+}
